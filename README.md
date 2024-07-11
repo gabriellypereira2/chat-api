40232c6c7058afbc471d73474956338b9c619835
Gabriellyereira <gabriellytauany57@gmail.com>
2024-07-11 17:00:03 -0300

    11/07/2024 - chat parte 2 e começo da 3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
+
 "#chat API" 
 =======
 # chat-api
->>>>>>> 79251f4e67b7792fa14ad42115b250f1c2e52cdd
+
---
+++ src/api.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express ();
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+const router = express.Router();
+app.use("/", router.get("/", async (req, res)=>{
+    res.status(200).send("<h1>API-CHAT</h1>")
+}));
+app.use("/", router.get("/sobre", async (req, res,)=>{
+    res.status(200).send({
+        "nome": "API-CHAT",
+        "versão": "0.0.1",
+        "autor": "Gabrielly Pereira"
+    })
+}));
+
+app.use("/", router.get("/salas", async (req, res,)=>{
+    const salaController = require('./controller/salaController');
+    const resp = await salaController.get();
+    res.status(200).send(resp);
+}));
+
+module.exports=app;
+
